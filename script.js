a69e03efa76bd3be6ac6804baa7fbe6db5f56cec
Debjit Datta <51264842+deb1401jit@users.noreply.github.com>
2023-03-17 18:27:11 +0530

    Files reuploaded

---
+++ script.js
@@ -0,0 +1,35 @@
+// =====MOBILE=====
+
+// Hamburger menu toggle
+const navbar = document.querySelector("nav"); 
+const menu = document.querySelector(".menu-button");
+const backgroundShadow = document.querySelector(".nav-background-shadow");
+menu.addEventListener('click', ()=>{
+  let visibilty = navbar.getAttribute("data-visible");
+  if(visibilty==="false"){
+    navbar.setAttribute("data-visible","true");
+    menu.children[0].src = "images/icon-close-menu.svg";
+    backgroundShadow.style.opacity=1;
+  } else {
+    navbar.setAttribute("data-visible","false");
+    menu.children[0].src = "images/icon-menu.svg";
+    backgroundShadow.style.opacity=0;
+  }
+});
+
+// Menu Dropdown
+const dropdownButton = document.querySelectorAll(".dropdown-button");
+dropdownButton.forEach(elm => {
+  elm.addEventListener('click',()=>{
+    let dropdownList = elm.nextElementSibling;
+    if(dropdownList.classList.contains("dropdown-active")){
+      dropdownList.classList.remove("dropdown-active");
+      elm.classList.remove("nav-active-button");
+      elm.children[0].src = "images/icon-arrow-down.svg";
+    } else {
+      dropdownList.classList.add("dropdown-active");
+      elm.classList.add("nav-active-button");
+      elm.children[0].src = "images/icon-arrow-up.svg";
+    }
+  });
+});
